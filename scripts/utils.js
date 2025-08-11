8911a80fdef4cbd206ee818575f6cf4df98f84cf
TheOnlyMoumen <theonlymoumen@gmail.com>
2025-08-12 00:40:31 +0100

    Fix a typo

--- scripts/utils.js
+++ scripts/utils.js
@@ -21,5 +21,5 @@ export function getFormattedDate(timestamp) {
   const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
 
-  return `${day}, ${month} ${day}, ${year} ${hours}:${minutes}`;
+  return `${weekDay}, ${month} ${day}, ${year} ${hours}:${minutes}`;
 }
