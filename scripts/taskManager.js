1b1bcc12a9e4260b68ec5f89723d135d921034aa
TheOnlyMoumen <theonlymoumen@gmail.com>
2025-08-12 00:42:41 +0100

    Add functionality to the info button
    - Clicking the info button pops up an info card.
    - A background blur appears when the card is displayed.

--- scripts/taskManager.js
+++ scripts/taskManager.js
@@ -26,6 +26,7 @@ export function renderTask(task) {
   const taskTitle = document.createElement("p");
   const infoBtn = document.createElement("button");
   const deleteBtn = document.createElement("button");
+  const infoCard = createInfoCard(task);
 
   taskElement.id = task.id;
   taskElement.classList.add("task");
@@ -48,7 +49,7 @@ export function renderTask(task) {
   deleteBtn.type = "button";
   deleteBtn.innerHTML = "<i class='fa-solid fa-trash'></i>";
 
-  taskElement.append(checkbox, taskTitle, infoBtn, deleteBtn);
+  taskElement.append(checkbox, taskTitle, infoBtn, deleteBtn, infoCard);
 
   dom.taskContainer.append(taskElement);
 }
@@ -65,11 +66,17 @@ export function addTaskEvents(task, taskMap) {
   const checkbox = taskElement.querySelector("label");
   const infoBtn = taskElement.querySelector(".info-btn");
   const deleteBtn = taskElement.querySelector(".delete-btn");
+  const infoCard = taskElement.querySelector(".info-card");
+  const exitBtn = taskElement.querySelector(".exit-btn");
 
   checkbox.addEventListener("change", event => {
     task.completed = event.target.checked;
     taskMap.get(task.id).completed = task.completed;
     saveTasks(taskMap);
+
+    infoCard.querySelector(".status").innerHTML = task.completed ? 
+      "<i class='fa-solid fa-check'></i> Completed" : 
+      "<i class='fa-solid fa-xmark'></i> Uncompleted";
     
     if (task.completed) {
       taskElement.classList.add("completed");
@@ -80,17 +87,57 @@ export function addTaskEvents(task, taskMap) {
     }
   });
 
-  /*
   infoBtn.addEventListener("click", () => {
-    displayInfoCard(task);
+    taskElement.querySelector(".info-card").classList.add("visible");
+    dom.root.classList.add("blur");
   });
-  */
 
   deleteBtn.addEventListener("click", () => {
     deleteTask(task, taskMap);
   });
+
+  exitBtn.addEventListener("click", () => {
+    infoCard.classList.remove("visible");
+    dom.root.classList.remove("blur");
+  });
 }
 
+export function createInfoCard(task) {
+  const cardElement = document.createElement("div");
+  const cardContent = document.createElement("div");
+  const exitBtn = document.createElement("button");
+  const taskStatus = task.completed ? 
+    "<i class='fa-solid fa-check'></i> Completed" : 
+    "<i class='fa-solid fa-xmark'></i> Uncompleted";
+
+  cardContent.innerHTML = `
+    <div>
+      <p><b>Creation date</b></p>
+      <p>${task.date}</p>
+    </div>
+    <div>
+      <p><b>Title</b></p>
+      <p>${task.taskTitle}</p>
+    </div>
+    <div>
+      <p><b>ID</b></p>
+      <p>${task.id.toString()}</p>
+    </div>
+    <div>
+      <p><b>Status</b></p>
+      <p class='status'>${taskStatus}</p>
+    </div>
+  `;
+
+  exitBtn.innerHTML = "<i class='fa-solid fa-xmark'></i>";
+  exitBtn.classList.add("exit-btn");
+  cardContent.append(exitBtn);
+  cardContent.classList.add("info-card-content");
+  cardElement.append(cardContent);
+  cardElement.classList.add("info-card");
+
+  return cardElement;
+}
 
 export function saveTasks(taskMap) {
   const taskList = Array.from(taskMap);
